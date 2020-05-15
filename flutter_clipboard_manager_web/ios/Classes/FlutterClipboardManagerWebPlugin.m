4eeece00388c164543183bdc14d39c5e5ac24edf
creativecreatorormaybenot <19204050+creativecreatorormaybenot@users.noreply.github.com>
2020-05-15 14:40:09 +0000

    Add plugin stuff

---
+++ flutter_clipboard_manager_web/ios/Classes/FlutterClipboardManagerWebPlugin.m
@@ -0,0 +1,15 @@
+#import "FlutterClipboardManagerWebPlugin.h"
+#if __has_include(<flutter_clipboard_manager_web/flutter_clipboard_manager_web-Swift.h>)
+#import <flutter_clipboard_manager_web/flutter_clipboard_manager_web-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_clipboard_manager_web-Swift.h"
+#endif
+
+@implementation FlutterClipboardManagerWebPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftFlutterClipboardManagerWebPlugin registerWithRegistrar:registrar];
+}
+@end
